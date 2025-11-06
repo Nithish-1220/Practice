deaa47d904f1886a87ba47d54b94f174463a7907
Nithish-1220 <nithish202007@gmail.com>
2025-11-06 23:39:26 +0530

    add new method

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
-public class HelloWorld{
-    public static void main(String[] args){
+public class HelloWorld {
+    public static void main(String[] args) {
         System.out.println("Hello World 1!");
+    }
+
+    public void dummy() {
+        System.out.println("dummy");
     }
 }
